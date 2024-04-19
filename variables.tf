e23faa94c6892d5cb3486a31cc01c01ddfa2ce84
ragerumal <126337647+ragerumal@users.noreply.github.com>
2024-04-19 12:11:06 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 
 variable "cluster_name" {
 type = string
-default = ["eks-demo-terraform-rag"]
+default = "eks-demo-terraform-rag"
 }
 
 variable "cluster_version" {
 type = string
-default = ["1.28"]
+default = "1.28"
 }
 
 variable "ami_release_version" {
 type = string
-default = ["1.28"]
+default = "1.28"
 
 }
